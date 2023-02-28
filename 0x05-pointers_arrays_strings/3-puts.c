4282e6a1dace81ba46d9fdf18e8dd1148cf629a3
OBORJACKSON <oborjackson@hotmail.com>
2023-02-28 10:05:15 -0800

    print string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include "stdio.h"
-#include "stdlib.h"
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
-
 void _puts(char *str)
 {
-	while(*str != '\0')
+	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
 		_putchar('\n');
 }
+
